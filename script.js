0380ce531424c0e304b3303fb440b9e94169c33b
Ygor <ygor_fernandes1996@hotmail.com>
2023-05-02 00:51:50 -0300

    Realizando entrega

--- script.js
+++ script.js
@@ -10,3 +10,34 @@ const seaBoard = [
 /*
   Desenvolva seu código logo abaixo
 */
+
+function allocateShips(shipPositions, initialBoard) {
+  for (let i = 0; i < shipPositions.length; i++) {
+    let currentShipPosition = shipPositions[i];
+    initialBoard[currentShipPosition[0]][currentShipPosition[1]] = "S";
+  }
+
+  return initialBoard;
+}
+
+function checkGuesses(guesses, mountedBoard) {
+  for (let i = 0; i < guesses.length; i++) {
+    let currentGuess = guesses[i];
+    if (mountedBoard[currentGuess[0]][currentGuess[1]] == "S") {
+      mountedBoard[currentGuess[0]][currentGuess[1]] = "X";
+    }
+  }
+  return mountedBoard;
+}
+
+function checkWinCondition(guessedBoard) {
+  for (let i = 0; i < guessedBoard.length; i++) {
+    let currentBoardLine = guessedBoard[i];
+    for (let j = 0; j < currentBoardLine.length; j++) {
+      if (currentBoardLine[j] == "S") {
+        return false;
+      }
+    }
+  }
+  return true;
+}
